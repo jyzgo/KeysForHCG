f29153348478e82f71e98f934b450643f0d29295
Chen0915 <dhcking1@gmail.com>
2021-08-02 17:38:32 +0800

    update ads id
    (cherry picked from commit 8e589a7147264e429e35196b36264aa315af5ee9)

--- KeyBase.cs
+++ KeyBase.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class KeyBase 
 {
-    public const string BUNDLE_ID = "";
-    public const string MAX_INTER_ID_A = "";
+    public const string BUNDLE_ID = "com.backfire.panda";
+    public const string MAX_INTER_ID_A = "3d1810f143b9b928";
     public const string MAX_INTER_ID_B = "";
-    public const string MAX_REWARD_ID = "";
+    public const string MAX_REWARD_ID = "7b274bb7a0b8fde1";
     
-    public const string  ADMOB_APP_ID= "";
-    public const string  ADJUST_TOKEN = "";
-    public const string  FACEBOOK_APP_ID = "";
+    public const string  ADMOB_APP_ID= "ca-app-pub-8119085811134312~4440570902";
+    public const string  ADJUST_TOKEN = "ckhrvnz38c1s";
+    public const string  FACEBOOK_APP_ID = "560369645388411";
 }

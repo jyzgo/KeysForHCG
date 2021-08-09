81ce2f27d0e579cc1bf3277dd53b14bcaddeeed4
DESKTOP-20CE13I\admin <dhcking1@gmail.com>
2021-08-09 17:53:15 +0800

    Iroom

--- KeyBase.cs
+++ KeyBase.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class KeyBase 
 {
-    public const string BUNDLE_ID = "com.backfire.shape";
-    public const string MAX_INTER_ID_A = "3d1810f143b9b928";
+    public const string BUNDLE_ID = "com.Backfire.iroom";
+    public const string MAX_INTER_ID_A = "f6193243ca9fbe81";
     public const string MAX_INTER_ID_B = "";
-    public const string MAX_REWARD_ID = "7b274bb7a0b8fde1";
+    public const string MAX_REWARD_ID = "e36012850d64dff5";
     
-    public const string  ADMOB_APP_ID= "ca-app-pub-8119085811134312~4440570902";
-    public const string  ADJUST_TOKEN = "ckhrvnz38c1s";
-    public const string  FACEBOOK_APP_ID = "560369645388411";
+    public const string  ADMOB_APP_ID= "ca-app-pub-8119085811134312~7545703240";
+    public const string  ADJUST_TOKEN = "5i58dgkyiygw";
+    public const string  FACEBOOK_APP_ID = "1001631193943609";
 }

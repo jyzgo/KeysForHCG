aff718cf1d199137d90adc687ff79dad3591aa27
DESKTOP-20CE13I\admin <dhcking1@gmail.com>
2021-08-09 18:32:59 +0800

    Dream Room

--- KeyBase.cs
+++ KeyBase.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class KeyBase 
 {
-    public const string BUNDLE_ID = "com.backfire.shape";
-    public const string MAX_INTER_ID_A = "3d1810f143b9b928";
+    public const string BUNDLE_ID = "com.backfire.dreamroom";
+    public const string MAX_INTER_ID_A = "1c4235a92f6c8aeb";
     public const string MAX_INTER_ID_B = "";
-    public const string MAX_REWARD_ID = "7b274bb7a0b8fde1";
+    public const string MAX_REWARD_ID = "a282232c49a86900";
     
-    public const string  ADMOB_APP_ID= "ca-app-pub-8119085811134312~4440570902";
-    public const string  ADJUST_TOKEN = "ckhrvnz38c1s";
-    public const string  FACEBOOK_APP_ID = "560369645388411";
+    public const string  ADMOB_APP_ID= "ca-app-pub-8119085811134312~7043974510";
+    public const string  ADJUST_TOKEN = "kikkyvxyky68";
+    public const string  FACEBOOK_APP_ID = "543221023491198";
 }

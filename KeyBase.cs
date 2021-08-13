6efac2a344b59f9b81f851e8207e64b4b56b5049
DESKTOP-20CE13I\admin <dhcking1@gmail.com>
2021-08-13 11:25:13 +0800

    Tug war

--- KeyBase.cs
+++ KeyBase.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class KeyBase 
 {
-    public const string BUNDLE_ID = "com.backfire.shape";
-    public const string MAX_INTER_ID_A = "3d1810f143b9b928";
+    public const string BUNDLE_ID = "com.backfire.tugwar";
+    public const string MAX_INTER_ID_A = "67196b8b4882bb16";
     public const string MAX_INTER_ID_B = "";
-    public const string MAX_REWARD_ID = "7b274bb7a0b8fde1";
+    public const string MAX_REWARD_ID = "da7288026d9c02b7";
     
-    public const string  ADMOB_APP_ID= "ca-app-pub-8119085811134312~4440570902";
-    public const string  ADJUST_TOKEN = "ckhrvnz38c1s";
-    public const string  FACEBOOK_APP_ID = "560369645388411";
+    public const string  ADMOB_APP_ID= "ca-app-pub-8119085811134312~7632105791";
+    public const string  ADJUST_TOKEN = "3riczfsud0zk";
+    public const string  FACEBOOK_APP_ID = "1220938288355163";
 }
